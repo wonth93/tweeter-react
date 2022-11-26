0c97e86c4679ec923e7d9c1c0f6039b54fdab8e9
wonth93 <wongth93@gmail.com>
2022-11-25 21:04:46 -0800

    disable submit behavior

---
+++ react-version/src/components/TweetForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function TweetForm() {
+  const tweetArea = "What are you humming about?";
+
+  return (
+    <section className="newtweet">
+      <form method="post" action="/tweets" className="newtweet__form" onClick={(event) => event.preventDefault()}>
+        <textarea className="form__textarea" name="text" placeholder={tweetArea}></textarea>
+        <input type="submit" value="Tweet" className="form__input" onClick={(event) => event.preventDefault()} />
+        <span className="form__counter">140</span>
+      </form>
+    </section>
+  );
+}
+
+export default TweetForm;
